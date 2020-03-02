46996a70ae96c90b4edc9575546b8c856caa5a28
gmoravaneni <gmoravaneni@prokarma.com>
2020-03-01 18:07:43 -0800

    Task 3: According to the business requirement providing a facility to the user to choose custom dates to view the trends within a specific period of time.
    Note: Need to remove the unwanted code related to the second favorite time period input field.

--- libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
+++ libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PriceQueryFacade } from '@coding-challenge/stocks/data-access-price-query';
-import { debounceTime } from 'rxjs/operators';
+import { debounceTime, skipWhile, map } from 'rxjs/operators';
 
 
 interface TimePeriod {
@@ -21,7 +21,13 @@ export class StocksComponent implements OnInit {
   symbol: string;
   display: string;
 
-  quotes$ = this.priceQuery.priceQueries$;
+  quotes$ = this.priceQuery.priceQueries$.pipe(map(data => {
+    const { fromDate, toDate } = this.stockPickerForm.value;
+    if(fromDate || toDate) {
+      return this.filterByDateSelection(data);
+    }
+    return data;
+  }));
 
   timePeriods: TimePeriod[]  = [
     { value: 'max', display: 'All available data'},
@@ -37,19 +43,26 @@ export class StocksComponent implements OnInit {
 
 
   constructor(private fb: FormBuilder, private priceQuery: PriceQueryFacade) {
-    this.stockPickerForm = fb.group({
+    this.stockPickerForm = this.fb.group({
       symbol: [null, Validators.required],
-      period: [null, Validators.required]
+      period: [null, Validators.required],
+      fromDate: [null],
+      toDate: [null]
     });
   }
 
   ngOnInit() {
     this.stockPickerForm.controls.symbol.valueChanges.pipe(debounceTime(500)).subscribe(() => this.fetchQuote());
+    const { symbol, fromDate, toDate } = this.stockPickerForm.controls;
+    symbol.valueChanges.pipe(debounceTime(400)).subscribe(() => this.fetchQuote());
+    fromDate.valueChanges.subscribe(() => this.validateDate('fromDate'));
+    toDate.valueChanges.subscribe(() => this.validateDate('toDate'));
+    this.stockPickerForm.valueChanges.pipe(skipWhile(() => !fromDate.value || !toDate.value)).subscribe(() => this.compareDates());
   }
 
   fetchQuote() {
     if (this.stockPickerForm.valid) {
-      const { symbol, period } = this.stockPickerForm.value;
+      const { symbol, period, fromDate, toDate} = this.stockPickerForm.value;
       this.priceQuery.fetchQuote(symbol, period);
     }
   }
@@ -57,4 +70,39 @@ export class StocksComponent implements OnInit {
   selectedDatePeriod() {
     this.fetchQuote();
   }
+
+  private validateDate(control: string) {
+    const date = this.stockPickerForm.controls[control];
+    const presentDay = new Date();
+    if(date.value > presentDay) {
+      date.setValue(presentDay);
+    }
+    this.fetchQuote();
+  }
+
+  private compareDates() {
+    const { fromDate, toDate } = this.stockPickerForm.controls;
+    if (fromDate.value && toDate.value) {
+      if (fromDate.value > toDate.value) {
+        toDate.setValue(fromDate.value);
+      }
+    }
+  }
+
+  private filterByDateSelection(items: any[]) {
+    const { fromDate, toDate } = this.stockPickerForm.value;
+    const filtered = items.filter(item => {
+      const date = new Date(item[0]);
+      if(fromDate && toDate) {
+        return date >= fromDate && date <= toDate;
+      } else if(fromDate) {
+        return date >= fromDate;
+      } else if(toDate) {
+        return date <= toDate;
+      }
+    });
+    return filtered;
+  }
 }
+
+
