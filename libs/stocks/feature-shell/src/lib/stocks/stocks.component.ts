5bfbd6328022c9b2857f0639166fbe94fe01ec93
gmoravaneni <gmoravaneni@prokarma.com>
2020-03-05 06:24:10 -0800

    Task 3: According to the business requirement providing a facility to the user to choose custom dates to view the trends within a selected period of time.

--- libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
+++ libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
@@ -1,60 +1,105 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PriceQueryFacade } from '@coding-challenge/stocks/data-access-price-query';
-import { debounceTime } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+import {CHART_DATE_RANGE} from '../../utils/stock-chart-constants';
 
-
-interface TimePeriod {
-  value: string;
-  display: string;
-}
 @Component({
   selector: 'coding-challenge-stocks',
   templateUrl: './stocks.component.html',
   styleUrls: ['./stocks.component.css']
 })
 
-export class StocksComponent implements OnInit {
+export class StocksComponent implements OnInit, OnDestroy {
   stockPickerForm: FormGroup;
-  value: string;
-  period: string;
-  symbol: string;
-  display: string;
-
+  fromDate: string;
+  toDate: string;
+  toDatedisabled = true;
   quotes$ = this.priceQuery.priceQueries$;
-
-  timePeriods: TimePeriod[]  = [
-    { value: 'max', display: 'All available data'},
-    { value: '5y', display: 'Five years'},
-    { value: '2y', display: 'Two years'},
-    { value: '1y', display: 'One year'},
-    { value: 'ytd', display: 'Year-to-date'},
-    { value: '6m', display: 'Six months'},
-    { value: '3m', display: 'Three months'},
-    { value: '1m', display: 'One month'}
-  ];
-
+  fromDateSub:Subscription;
+  max = new Date();
 
 
   constructor(private fb: FormBuilder, private priceQuery: PriceQueryFacade) {
-    this.stockPickerForm = fb.group({
+    this.stockPickerForm = this.fb.group({
       symbol: [null, Validators.required],
-      period: [null, Validators.required]
+      fromDate: [null, Validators.required],
+      toDate: [null, Validators.required]
+    }, {
+      validator: this.toDateSelectionValidator('fromDate', 'toDate')
     });
   }
 
   ngOnInit() {
-    this.stockPickerForm.controls.symbol.valueChanges.pipe(debounceTime(500)).subscribe(() => this.fetchQuote());
+    this.stockPickerForm.controls['toDate'].disable()
+    this.fromDateSub = this.stockPickerForm.controls.fromDate.valueChanges.subscribe(() => this.stockPickerForm.controls['toDate'].enable());
   }
+
 
   fetchQuote() {
     if (this.stockPickerForm.valid) {
-      const { symbol, period } = this.stockPickerForm.value;
-      this.priceQuery.fetchQuote(symbol, period);
+      const { symbol, fromDate, toDate } = this.stockPickerForm.value;
+      const period = this.dateRangeDifference(toDate, fromDate);
+      if(!period) {
+        this.stockPickerForm.controls['fromDate'].setErrors({ 'incorrect': true });
+        this.stockPickerForm.controls['toDate'].setErrors({ 'incorrect': true });
+      } else {
+        this.priceQuery.fetchQuote(symbol, period);
+    }
+  }
+  }
+
+  dateRangeDifference (toDate: Date, fromDate: Date) {
+    const toDateValue = toDate.valueOf();
+    const fromDateValue = fromDate.valueOf();
+    const days = (toDateValue !== fromDateValue) ? Math.round(Math.abs((toDateValue - fromDateValue) / 86400000)) : 0;
+    let period;
+    switch (true) {
+      case (days === 0 ):
+        return null;
+      case (days <= 2):
+        period = CHART_DATE_RANGE.PERIOD['1D'];
+        break;
+      case (days <= 5):
+        period = CHART_DATE_RANGE.PERIOD['5D'];
+        break;
+      case (days <= 15):
+        period = CHART_DATE_RANGE.PERIOD['15D'];
+        break;
+      case (days <= 30):
+        period = CHART_DATE_RANGE.PERIOD['1M'];
+        break;
+      case (days <= 180):
+        period = CHART_DATE_RANGE.PERIOD['6M'];
+        break;
+      case (days <= 365):
+        period = CHART_DATE_RANGE.PERIOD['1Y'];
+        break;
+      case (days > 730):
+        period = CHART_DATE_RANGE.PERIOD['MAX'];
+        break;
+    }
+    return period;
+  }
+
+
+  toDateSelectionValidator(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      const fromDate = formGroup.controls[controlName];
+      const toDate = formGroup.controls[matchingControlName];
+      if (toDate.errors && toDate.errors.matDatepickerMin) {
+        toDate.setValue(fromDate.value);
+      }
     }
   }
 
-  selectedDatePeriod() {
-    this.fetchQuote();
+  dateSelection() {
+    console.log('change event');
+    this.toDatedisabled = false
   }
+
+  ngOnDestroy() {
+    this.fromDateSub.unsubscribe();
+  }
+
 }
